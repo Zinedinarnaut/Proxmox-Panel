81c13a30eb964ec3d032519562e9ce0dbbe68bdd
zined <zinedinarnaut085@gmail.com>
2025-02-02 16:24:49 +1100

    update

--- proxmox_connection.py
+++ proxmox_connection.py
@@ -7,7 +7,7 @@ def get_proxmox():
     Creates and returns a ProxmoxAPI object.
     Adjust host/user/pass or read from environment variables as needed.
     """
-    PROXMOX_HOST = os.getenv("PROXMOX_HOST", "192.168.0.21")
+    PROXMOX_HOST = os.getenv("PROXMOX_HOST", "YOUR_IP")
     PROXMOX_USER = os.getenv("PROXMOX_USER", "root@pam")
-    PROXMOX_PASS = os.getenv("PROXMOX_PASS", "151677")
+    PROXMOX_PASS = os.getenv("PROXMOX_PASS", "YOUR_PASSWORD")
     return ProxmoxAPI(PROXMOX_HOST, user=PROXMOX_USER, password=PROXMOX_PASS, verify_ssl=False)
